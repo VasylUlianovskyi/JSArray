c5540976ed8ec488506c75035020a76703b16868
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-05-28 01:33:36 +0300

    Done task of array

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,1 +1,82 @@
+// 0 Створити числовий масив та проініціалізувати його (*випадковими числами).
+// 1 Видалити останній і початковий елемент з масиву, додати елемент до початку і кінця.
+// 2 Вивести розмір масиву.
+// 3 Зробити копію масиву.
 
+// Не використовувати методи перебору масивів (forEach, filter, map, findIndex), а використати цикли
+// 4 Вивести елементи з парними індексами. ///
+// 5 Знайти добуток елементів масиву ///
+
+const numbers = [1, 2, 3, -6, -3, 10, 0];
+
+console.log(numbers);
+
+const numbers2 = [...numbers];
+numbers2.shift();
+numbers2.pop();
+numbers2.unshift(-3);
+numbers2.push(-5);
+
+console.log("numbers >>", numbers);
+console.log("Розмір масиву =", numbers.length);
+
+console.log("numbers2 >>", numbers2);
+
+let pairIndex = [];
+for (let i = 0; i < numbers.length; i += 2) {
+  pairIndex.push(numbers[i]);
+}
+
+console.log(pairIndex);
+
+function numbersMult(num) {
+  let sum = 1;
+  for (let i = 0; i < num.length; i++) {
+    sum *= num[i];
+  }
+  return sum;
+}
+
+console.log(numbersMult(numbers));
+
+// Методи перебору масивів (forEach, filter, map, findIndex, *some, *every).
+// 8 Отримати новий масив із заданого, який міститиме лише ненульові числа (-1, 5, 0, 9, -10 => -1, 5, 9, -10). // filter
+
+const possitiveNumbers = numbers.filter((number) => number >= 0);
+console.log(possitiveNumbers);
+
+// 9 Отримати новий масив їх заданого, який міститиме всі елементи вихідного, поділені на 100 (99, 5, 0, 9, 30 => 0.99, 0.05, 0, 0.09, 0.3). // map
+
+const numbersMultipy100 = numbers.map((number) => number * 100);
+console.log(numbersMultipy100);
+
+// 10 Вивести елементи масиву, зведені у куб. // forEach
+
+const numbersCubed = [];
+numbers.forEach((number) => {
+  const inCube = number ** 3;
+  numbersCubed.push(inCube);
+});
+
+console.log(numbersCubed);
+
+// 11 Визначити індекс елемента, квадрат якого дорівнює 100, і видалити його, або видати діагностичне повідомлення, якщо такого елементу не існує. // findIndex
+
+const numbersSquared100 = numbers.findIndex(
+  (number) => number * number === 100
+);
+console.log(numbersSquared100);
+numbers.splice(5, 1);
+console.log(numbers);
+
+// 12 *Перевірити, чи всі елементи масиву є парними числами (* або простими числами). // every
+
+const isSimpleNumbers = numbers.every(
+  (number) => number % number === 0 && number % 1 === 0
+);
+console.log(isSimpleNumbers);
+
+//13 *Перевірити, чи є у масиві бодай один від'ємний елемент. // some
+
+const isNegNumber = numbers.some((number) => number < 0);
+console.log(isNegNumber);
---
+++ assets/js/arrayPhones.js
@@ -0,0 +1,144 @@
+// 6 Задано масив з описом телефонів з полями id, brand, model, color, price, RAM, ... (можна обрати будь-яку іншу сутність). Можна згенерувати дані за допомогою ШІ.
+// - Сформувати розмітку для карток. (*Застилізувати картки.)
+// - Знайти середню ціну телефонів.
+// - *Знайти кількість телефонів з RAM 4, 6, 8, 12 ГБ (можна спробувати накопичити дані в об'єкт вигляду: ключ - обсяг RAM, значення - кількість телефонів з цим обсягом RAM).
+
+const phones = [
+  {
+    id: 1,
+    brand: "Apple",
+    model: "iPhone 14",
+    color: "Black",
+    price: 999,
+    RAM: 6,
+    image: "https://content1.rozetka.com.ua/goods/images/big/284913535.jpg",
+  },
+  {
+    id: 2,
+    brand: "Samsung",
+    model: "Galaxy S23",
+    color: "White",
+    price: 899,
+    RAM: 8,
+    image: "https://content.rozetka.com.ua/goods/images/big/320499894.jpg",
+  },
+  {
+    id: 3,
+    brand: "Google",
+    model: "Pixel 7",
+    color: "Green",
+    price: 799,
+    RAM: 8,
+    image: "https://content2.rozetka.com.ua/goods/images/big/417935859.jpg",
+  },
+  {
+    id: 4,
+    brand: "OnePlus",
+    model: "9 Pro",
+    color: "Silver",
+    price: 699,
+    RAM: 12,
+    image: "https://content2.rozetka.com.ua/goods/images/big/194862977.jpg",
+  },
+  {
+    id: 5,
+    brand: "Xiaomi",
+    model: "Mi 11",
+    color: "Blue",
+    price: 599,
+    RAM: 6,
+    image: "https://content1.rozetka.com.ua/goods/images/big/160643974.jpg",
+  },
+  {
+    id: 6,
+    brand: "Oppo",
+    model: "Find X7",
+    color: "Brown",
+    price: 549,
+    RAM: 8,
+    image: "https://content.rozetka.com.ua/goods/images/big/414753872.jpg",
+  },
+  {
+    id: 7,
+    brand: "Sony",
+    model: "Xperia 5 III",
+    color: "Red",
+    price: 649,
+    RAM: 8,
+    image: "https://content.rozetka.com.ua/goods/images/big/221214566.jpg",
+  },
+  {
+    id: 8,
+    brand: "Huawei",
+    model: "P50",
+    color: "Gold",
+    price: 699,
+    RAM: 8,
+    image: "https://content2.rozetka.com.ua/goods/images/big/275435381.jpg",
+  },
+  {
+    id: 9,
+    brand: "Asus",
+    model: "ROG Phone 6",
+    color: "Black",
+    price: 999,
+    RAM: 12,
+    image: "https://content.rozetka.com.ua/goods/images/big/433900663.jpg",
+  },
+  {
+    id: 10,
+    brand: "Realme",
+    model: "GT Master",
+    color: "Grey",
+    price: 399,
+    RAM: 6,
+    image: "https://content1.rozetka.com.ua/goods/images/big/279053385.jpg",
+  },
+  {
+    id: 11,
+    brand: "Nokia",
+    model: "G50",
+    color: "Grey",
+    price: 349,
+    image: "https://content2.rozetka.com.ua/goods/images/big/363240555.jpg",
+    RAM: 4,
+  },
+  {
+    id: 12,
+    brand: "Motorola",
+    model: "Edge 30",
+    color: "White",
+    price: 499,
+    RAM: 6,
+    image: "https://images.prom.ua/4371410327_w528_h473_motorola-edge-30.jpg",
+  },
+];
+
+function addPhone(phone) {
+  document.write(`
+  <article class='phone-card'>
+  <img class="phone-img" src=${phone.image} alt="">
+      <h2 class='title'>${phone.brand} ${phone.model}</h2>
+      <p class='discriprion'>Color: ${phone.color}</p>
+      <p class='discriprion'>RAM: ${phone.RAM}GB</p>
+      <p class='discriprion'>Price: $${phone.price}</p>
+      <button class = 'btn'>BUY!</button>
+    </article>
+  `);
+}
+
+document.write("<section class='card-list'>");
+phones.forEach(addPhone);
+document.write("</section>");
+
+// - Знайти середню ціну телефонів.
+
+function midllePrice(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i].price;
+  }
+  return sum / arr.length;
+}
+
+console.log("Cередня ціна телефона:", midllePrice(phones), "$");
